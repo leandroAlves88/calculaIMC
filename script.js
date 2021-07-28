cb118162238d2184e7c1b79b248fba5b451e525f
leandroAlves88 <lalves.ferreira99@gmail.com>
2021-07-27 23:23:39 -0300

    Correção no calculo IMC e exibição de mensagem da faixa do IMC

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function calculaImc(){
-    let valorPeso =  document.getElementById("textPeso");
-    let valorAltura =  document.getElementById("textAltura");
-    let Imc =  valorPeso/valorAltura*2;
+    let valorPeso =  document.getElementById("textPeso").value;
+    let valorAltura =  document.getElementById("textAltura").value;
+    let calculo =  valorPeso/(valorAltura*valorAltura);
+    let imc = calculo.toFixed(2);
 
-    document.getElementById("lblTextNome").innerHTML = Imc;
+    console.log("Valor peso "+valorPeso);
+    console.log("Valor altura "+valorAltura);
+    console.log("imc "+imc);
+
+    if(isNaN(imc)){
+        document.getElementById("lblTextNome").innerHTML = "Erro";
+    }else if(imc > 30){
+        document.getElementById("lblTextNome").innerHTML = imc +" - Obesidade";
+    }else if(imc > 24.9 && imc <=30){
+        document.getElementById("lblTextNome").innerHTML = imc +" - Sobrepeso";
+    }else if(imc > 18.5 && imc <=24.9){
+        document.getElementById("lblTextNome").innerHTML = imc +" - Normal";
+    }else if(imc < 18.5 ){
+        document.getElementById("lblTextNome").innerHTML = imc +" - Magreza";
+    }
+
 }
